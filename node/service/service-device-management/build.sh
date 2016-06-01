e35631cedd4c6aa8d0436326074999700b28914a
Henryk Konsek <hekonsek@gmail.com>
2016-06-01 17:00:27 +0200

    Added device management service.

---
+++ node/service/service-device-management/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker build -t smolok/service-loader:0.0.0-SNAPSHOT .
---
+++ node/service/service-device-management/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker run --net=host -it smolok/service-loader:0.0.0-SNAPSHOT
