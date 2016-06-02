616aab8b0e50f041ea4982c78b22c1f32df82a03
Henryk Konsek <hekonsek@gmail.com>
2016-06-02 08:34:55 +0200

    Fixed Docker access rights.

--- node/service/loader/build-32.sh
+++ node/service/loader/build-32.sh
@@ -2,6 +2,6 @@
 
 npm install
 sudo docker build -t smolok/service-loader-32:0.0.0-SNAPSHOT -f Dockerfile-32 .
-docker tag -f smolok/service-loader-32:0.0.0-SNAPSHOT smolok/service-loader-32:latest
-docker push smolok/service-loader-32:0.0.0-SNAPSHOT
-docker push smolok/service-loader-32:latest
+sudo docker tag -f smolok/service-loader-32:0.0.0-SNAPSHOT smolok/service-loader-32:latest
+sudo docker push smolok/service-loader-32:0.0.0-SNAPSHOT
+sudo docker push smolok/service-loader-32:latest
