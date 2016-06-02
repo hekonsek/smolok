8d9be3344ada34794717d4e78266c71f113f2c6e
Henryk Konsek <hekonsek@gmail.com>
2016-06-02 08:24:54 +0200

    Service loader should listen on device channel.

--- node/service/service-loader/index.js
+++ node/service/service-loader/index.js
@@ -1,6 +1,7 @@
 var spawn = require('child_process').spawn;
 var container = require('rhea');
 
+var deviceId = process.env.DEVICE_ID || 'device-' + Math.random();
 var host = process.env.EVENT_BUS_SERVICE_HOST || 'localhost';
 
 container.on('message', function (context) {
@@ -8,9 +9,9 @@ container.on('message', function (context) {
     var dockerCommand = service.uri;
     dockerCommand = dockerCommand.replace('docker:', '');
     var segments = dockerCommand.split(' ');
-    var ls = spawn('docker', ['run', '--net=host', '--privileged', '-d'].concat(segments));
+    var dockerProcess = spawn('docker', ['run', '--net=host', '--privileged', '-d'].concat(segments));
 
-    ls.stdout.on('data', (data) => {
+    dockerProcess.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
         var fs = require('fs');
 
@@ -18,16 +19,14 @@ container.on('message', function (context) {
             if (err) {
                 return console.log(err);
             }
-
-            console.log('Added to /etc/rc.local');
+            console.log('Added to /etc/rc.local .');
         });
     });
-
-
-    ls.stderr.on('data', (data) => {
+    
+    dockerProcess.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
     });
 });
 
 var connection = container.connect({'host': host, 'port': 5672});
-connection.attach_receiver('loader/load');
+connection.attach_receiver('loader/load/deviceId');
