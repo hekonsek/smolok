2b4bd064d8d5d81093e7aae47afb2f86fce2c36d
Henryk Konsek <hekonsek@gmail.com>
2016-06-23 09:01:10 +0200

    Mavenized Spark-base Docker image.

--- spark/docker/spark-base/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t smolok/spark-base:0.0.0-SNAPSHOT .
-docker tag -f smolok/spark-base:0.0.0-SNAPSHOT smolok/spark-base:latest
-docker push smolok/spark-base:0.0.0-SNAPSHOT
-docker push smolok/spark-base:latest
--- spark/docker/spark-base/submit.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/spark/bin/spark-submit "$@"
