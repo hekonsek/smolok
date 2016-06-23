059418c79dc3dc75358b28daafb9b47b59d07c44
Henryk Konsek <hekonsek@gmail.com>
2016-06-23 09:15:26 +0200

    Mavenized Spark master build.

--- spark/docker/spark-standalone-master/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t smolok/spark-standalone-master:0.0.0-SNAPSHOT .
-docker tag -f smolok/spark-standalone-master:0.0.0-SNAPSHOT smolok/spark-standalone-master:latest
-docker push smolok/spark-standalone-master:0.0.0-SNAPSHOT
-docker push smolok/spark-standalone-master:latest
