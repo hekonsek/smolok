586a550dd440a9d6c321ad301d2471962a3ad996
Henryk Konsek <hekonsek@gmail.com>
2016-06-24 10:21:24 +0200

    Added Spark worker image building to Maven pipeline.

--- spark/docker/spark-standalone-worker/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t smolok/spark-standalone-worker:0.0.0-SNAPSHOT .
-docker tag -f smolok/spark-standalone-worker:0.0.0-SNAPSHOT smolok/spark-standalone-worker:latest
-docker push smolok/spark-standalone-worker:0.0.0-SNAPSHOT
-docker push smolok/spark-standalone-worker:latest
