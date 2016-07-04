70932b426e8255f33b8ff4e3077a1d2dba8580ba
Henryk Konsek <hekonsek@gmail.com>
2016-07-04 22:13:15 +0200

    Spark master should be configurable #71

--- cmd/src/main/groovy/smolok/cmd/commands/SparkStartCommand.groovy
+++ cmd/src/main/groovy/smolok/cmd/commands/SparkStartCommand.groovy
@@ -46,14 +46,19 @@ class SparkStartCommand implements Command {
             masterUrl = masterUrl.replaceFirst(/--master=/, '')
         }
 
+        def masterInterface = inputCommand.find{ it.startsWith('--master-interface=') }
+        if(masterInterface == null) {
+            masterInterface = 'localhost'
+        }
+
         if(inputCommand.length < 3) {
             LOG.debug('No node type specified - starting master and worker nodes...')
-            startSparkNode(outputSink, smolokVersion.get(), 'master', masterUrl)
-            startSparkNode(outputSink, smolokVersion.get(), 'worker', masterUrl)
+            startSparkNode(outputSink, smolokVersion.get(), 'master', masterUrl, masterInterface)
+            startSparkNode(outputSink, smolokVersion.get(), 'worker', masterUrl, masterInterface)
         } else if(inputCommand[2] == 'master') {
-            startSparkNode(outputSink, smolokVersion.get(), 'master', masterUrl)
+            startSparkNode(outputSink, smolokVersion.get(), 'master', masterUrl, masterInterface)
         } else if(inputCommand[2] == 'worker') {
-            startSparkNode(outputSink, smolokVersion.get(), 'worker', masterUrl)
+            startSparkNode(outputSink, smolokVersion.get(), 'worker', masterUrl, masterInterface)
         } else {
             throw new RuntimeException("Unknown Spark node type: ${inputCommand[2]}")
         }
@@ -61,9 +66,9 @@ class SparkStartCommand implements Command {
 
     // Private helpers
 
-    private void startSparkNode(OutputSink outputSink, String imageVersion, String nodeType, String masterUrl) {
+    private void startSparkNode(OutputSink outputSink, String imageVersion, String nodeType, String masterUrl, String masterInterface) {
         LOG.debug('Starting Spark node: {}', nodeType)
-        switch(new SparkClusterManager(docker).startSparkNode(imageVersion, nodeType, masterUrl)) {
+        switch(new SparkClusterManager(docker).startSparkNode(imageVersion, nodeType, masterUrl, masterInterface)) {
             case alreadyRunning:
                 outputSink.out("Spark ${nodeType} is already running. No need to start it.")
                 break
@@ -84,13 +89,15 @@ class SparkStartCommand implements Command {
             this.docker = docker
         }
 
-        ServiceStartupStatus startSparkNode(String imageVersion, String nodeType, String masterUrl) {
+        ServiceStartupStatus startSparkNode(String imageVersion, String nodeType, String masterUrl, String masterInterface) {
             LOG.debug('Starting Spark node: {}', nodeType)
             def containerBuilder = new ContainerBuilder("smolok/spark-standalone-${nodeType}:${imageVersion}").
                     name("spark-${nodeType}").net('host').
                     volumes(['/var/smolok/spark/jobs': '/var/smolok/spark/jobs'])
             if(masterUrl != null) {
                 containerBuilder.environment([SPARK_MASTER: masterUrl])
+            } else if(masterInterface != null) {
+                containerBuilder.environment([MASTER_INTERFACE: masterInterface])
             }
             def container = containerBuilder.build()
             docker.startService(container)
