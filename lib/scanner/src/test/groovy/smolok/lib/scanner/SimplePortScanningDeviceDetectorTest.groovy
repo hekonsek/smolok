37939395b6ae24249feb1faa977acdd145034c3a
Henryk Konsek <hekonsek@gmail.com>
2016-07-06 17:09:29 +0200

    Added extra tests.

---
+++ lib/scanner/src/test/groovy/smolok/lib/scanner/SimplePortScanningDeviceDetectorTest.groovy
@@ -0,0 +1,37 @@
+package smolok.lib.scanner
+
+import io.rhiot.scanner.SimplePortScanningDeviceDetector
+import org.junit.Assert
+import org.junit.Test
+import smolok.lib.ssh.server.NoneCredentialsPasswordAuthenticator
+import smolok.lib.ssh.server.SshServerBuilder
+
+import static com.google.common.truth.Truth.assertThat
+
+class SimplePortScanningDeviceDetectorTest extends Assert {
+
+    def detector = new SimplePortScanningDeviceDetector();
+
+    @Test
+    void shouldReachDevice() throws IOException {
+        def sshd = new SshServerBuilder().build().start()
+        def address = detector.detectDevices(sshd.port()).first()
+        def ssh = new Socket(address.address(), sshd.port())
+        assertThat(ssh.isConnected()).isTrue()
+        sshd.stop()
+    }
+
+    @Test
+    void shouldRejectNotAuthenticableDevice() {
+        // Given
+        def sshd = new SshServerBuilder().authenticator(new NoneCredentialsPasswordAuthenticator()).build().start()
+
+        // When
+        def devices = detector.detectDevices(sshd.port())
+
+        // Then
+        assertThat(devices).isEmpty()
+        sshd.stop()
+    }
+
+}
