706f369ce2877d7a44e0f5cfa5a726cf7a699acf
Henryk Konsek <hekonsek@gmail.com>
2016-07-19 09:56:54 +0200

    Added base Docker image to build.

--- spark/docker/spark-submit/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t smolok/spark-submit:0.0.0-SNAPSHOT .
-docker tag -f smolok/spark-submit:0.0.0-SNAPSHOT smolok/spark-submit:latest
-docker push smolok/spark-submit:0.0.0-SNAPSHOT
-docker push smolok/spark-submit:latest
