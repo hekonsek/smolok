1e80072a8570b0b2f51beea35d5f03950c884b94
Henryk Konsek <hekonsek@gmail.com>
2016-07-21 10:33:53 +0200

    Using Smolok data directory.

--- spark/archetypes/rdd/src/main/groovy/smolok/spark/archetype/rdd/MySparkJob.groovy
+++ spark/archetypes/rdd/src/main/groovy/smolok/spark/archetype/rdd/MySparkJob.groovy
@@ -7,7 +7,7 @@ class MySparkJob {
     static def job = new RddJobContext()
 
     static void main(String... args) {
-        def rdd = job.source('text-file:/var/data/foo.txt')
+        def rdd = job.source('text-file:/var/smolok/spark/data/foo.txt')
         def result = rdd.map{ String line -> line.toInteger() }.fold(0){ a, b ->
             a + b
         }
