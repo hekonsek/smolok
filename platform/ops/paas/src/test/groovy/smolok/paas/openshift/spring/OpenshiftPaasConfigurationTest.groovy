611aaadc3705662514a8165bdfce7a680ca0bd1b
Henryk Konsek <hekonsek@gmail.com>
2016-09-14 12:33:57 +0200

    Upgraded tests setup.

--- platform/ops/paas/src/test/groovy/smolok/paas/openshift/spring/OpenshiftPaasConfigurationTest.groovy
+++ platform/ops/paas/src/test/groovy/smolok/paas/openshift/spring/OpenshiftPaasConfigurationTest.groovy
@@ -4,15 +4,15 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.SpringApplicationConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 import smolok.bootstrap.Smolok
 import smolok.paas.Paas
 
 import static org.assertj.core.api.Assertions.assertThat
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = [Smolok.class, OpenshiftPaasConfiguration.class])
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Smolok.class)
 class OpenshiftPaasConfigurationTest {
 
     // Collaborators fixtures
