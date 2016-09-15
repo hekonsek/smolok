025b2ef3db16f07ccfe3ccd27b4b21fabda4de49
Henryk Konsek <hekonsek@gmail.com>
2016-09-15 12:26:28 +0200

    Added debug statement.

--- platform/ops/paas/src/main/groovy/smolok/paas/openshift/OpenshiftPaas.groovy
+++ platform/ops/paas/src/main/groovy/smolok/paas/openshift/OpenshiftPaas.groovy
@@ -103,9 +103,10 @@ class OpenshiftPaas implements Paas {
                 def serverPath = Paths.get(downloadManager.downloadedFile(OPENSHIFT_DISTRO).absolutePath, OPENSHIFT_DISTRO, 'openshift').toFile().absolutePath
                 processManager.executeAsync('sudo', serverPath, 'start')
                 await().atMost(60, SECONDS).until({isNotLoggedIntoProject()} as Callable<Boolean>)
-                await().atMost(120, SECONDS).until({
-                    !oc('login https://localhost:8443 -u admin -p admin --insecure-skip-tls-verify=true').first().
-                            startsWith('Error from server: User "admin" cannot get users at the cluster scope')
+                await().atMost(60, SECONDS).until({
+                    def x = oc('login https://localhost:8443 -u admin -p admin --insecure-skip-tls-verify=true').first()
+                    println x
+                            !x.startsWith('Error from server: User "admin" cannot get users at the cluster scope')
                 } as Callable<Boolean>)
                 oc('new-project smolok')
                 await().atMost(60, SECONDS).until({isOsStarted()} as Callable<Boolean>)
