9fbd1900f686ebb83fc7a9b60b66ac9b96ab2f93
Henryk Konsek <hekonsek@gmail.com>
2016-09-30 07:17:37 +0200

    Event bus persistence should be configurable.

--- platform/ops/eventbus/src/main/groovy/smolok/eventbus/spring/EventBusConfiguration.groovy
+++ platform/ops/eventbus/src/main/groovy/smolok/eventbus/spring/EventBusConfiguration.groovy
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Smolok under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package smolok.eventbus.spring
 
 import org.apache.activemq.broker.BrokerService
@@ -14,9 +30,10 @@ class EventBusConfiguration {
     @Bean(initMethod = 'start', destroyMethod = 'stop')
     BrokerService broker(
             @Value('${EVENTBUS_SERVICE_HOST:0.0.0.0}') String host,
-            @Value('${EVENTBUS_SERVICE_PORT:5672}') int port) {
+            @Value('${EVENTBUS_SERVICE_PORT:5672}') int port,
+            @Value('${EVENTBUS_PERSISTENCE:false}') boolean persistence) {
         def broker = new BrokerService()
-        broker.setPersistent(false)
+        broker.setPersistent(persistence)
         broker.addConnector("amqp://${host}:${port}")
         broker
     }
