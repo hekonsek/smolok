890ce6696c23bcd1aee390eb84ecd0c7b16aee68
Henryk Konsek <hekonsek@gmail.com>
2016-10-18 14:50:47 +0200

    Refactoring.

--- platform/lib/vertx/src/main/groovy/smolok/lib/vertx/AmqpProbe.groovy
+++ platform/lib/vertx/src/main/groovy/smolok/lib/vertx/AmqpProbe.groovy
@@ -3,20 +3,15 @@ package smolok.lib.vertx
 import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
 import io.vertx.core.Vertx
-import io.vertx.proton.ProtonClient
-import io.vertx.proton.ProtonConnection
-import io.vertx.proton.ProtonDelivery
-import io.vertx.proton.ProtonReceiver
-import io.vertx.proton.ProtonSession
-import org.apache.commons.lang3.Validate
+import io.vertx.proton.*
 import org.apache.qpid.proton.amqp.messaging.AmqpValue
-import org.apache.qpid.proton.amqp.messaging.Header
 import org.apache.qpid.proton.message.Message
 import smolok.lib.common.Uuids
 
 import java.util.concurrent.CountDownLatch
 
 import static java.util.concurrent.TimeUnit.SECONDS
+import static org.apache.commons.lang3.Validate.isTrue
 
 class AmqpProbe {
 
@@ -35,68 +30,11 @@ class AmqpProbe {
     }
 
     boolean canSendMessageTo(String host, int port) {
-        def responseAvailable = new CountDownLatch(1)
-        def delivered = false
-
-        connect(host, port, new Handler<AsyncResult<ProtonConnection>>() {
-            @Override
-            void handle(AsyncResult<ProtonConnection> connectionResponse) {
-                if(connectionResponse.succeeded()) {
-                    def message = Message.Factory.create()
-                    message.body = new AmqpValue('ping')
-                    connectionResponse.result().open().
-                            createSender(STATUS_CHANNEL).open().send(message, new Handler<ProtonDelivery>() {
-                        @Override
-                        void handle(ProtonDelivery protonDelivery) {
-                            delivered = true
-                            responseAvailable.countDown()
-                        }
-                    })
-                } else {
-                    responseAvailable.countDown()
-                }
-            }
-        })
-
-        try {
-            responseAvailable.await(15, SECONDS)
-            delivered && responseAvailable.count == 0
-        } catch (InterruptedException e) {
-            false
-        }
+        amqpExchange(host, port, STATUS_CHANNEL, 'ping')
     }
 
     void send(String host, int port, String channel, Object body) {
-        def responseAvailable = new CountDownLatch(1)
-        def delivered = false
-
-        connect(host, port, new Handler<AsyncResult<ProtonConnection>>() {
-            @Override
-            void handle(AsyncResult<ProtonConnection> connectionResponse) {
-                if(connectionResponse.succeeded()) {
-                    def message = Message.Factory.create()
-                    message.setAddress(channel)
-                    message.body = new AmqpValue(body)
-                    connectionResponse.result().open().
-                            createSender(channel).open().send(message, new Handler<ProtonDelivery>() {
-                        @Override
-                        void handle(ProtonDelivery protonDelivery) {
-                            delivered = true
-                            responseAvailable.countDown()
-                        }
-                    })
-                } else {
-                    responseAvailable.countDown()
-                }
-            }
-        })
-
-        try {
-            responseAvailable.await(15, SECONDS)
-            Validate.isTrue(delivered)
-        } catch (InterruptedException e) {
-            Validate.isTrue(delivered)
-        }
+        isTrue(amqpExchange(host, port, channel, body))
     }
 
     Object request(String host, int port, String channel, Object body) {
@@ -149,6 +87,39 @@ class AmqpProbe {
     }
 
     // Helpers
+
+    private boolean amqpExchange(String host, int port, String channel, Object body) {
+        def responseAvailable = new CountDownLatch(1)
+        def delivered = false
+
+        connect(host, port, new Handler<AsyncResult<ProtonConnection>>() {
+            @Override
+            void handle(AsyncResult<ProtonConnection> connectionResponse) {
+                if(connectionResponse.succeeded()) {
+                    def message = Message.Factory.create()
+                    message.setAddress(channel)
+                    message.body = new AmqpValue(body)
+                    connectionResponse.result().open().
+                            createSender(channel).open().send(message, new Handler<ProtonDelivery>() {
+                        @Override
+                        void handle(ProtonDelivery protonDelivery) {
+                            delivered = true
+                            responseAvailable.countDown()
+                        }
+                    })
+                } else {
+                    responseAvailable.countDown()
+                }
+            }
+        })
+
+        try {
+            responseAvailable.await(15, SECONDS)
+            delivered && responseAvailable.count == 0
+        } catch (InterruptedException e) {
+            false
+        }
+    }
 
     private void connect(String host, int port, Handler<AsyncResult<ProtonConnection>> connectionHandler) {
         ProtonClient.create(vertx).connect(host, port, connectionHandler)
