06239ca3bf856ea935113858224e930494649dad
Henryk Konsek <hekonsek@gmail.com>
2016-11-07 13:11:42 +0100

    Refactoring.

--- platform/lib/docker/src/test/groovy/smolok/lib/docker/spring/DockerConfigurationTest.groovy
+++ platform/lib/docker/src/test/groovy/smolok/lib/docker/spring/DockerConfigurationTest.groovy
@@ -1,30 +1,47 @@
+/**
+ * Licensed to the Smolok under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package smolok.lib.docker.spring
 
 import org.eclipse.kapua.locator.spring.KapuaApplication
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.SpringApplicationConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 import smolok.lib.docker.ContainerBuilder
 import smolok.lib.docker.ContainerStatus
 import smolok.lib.docker.Docker
 
-import static java.util.UUID.randomUUID
 import static org.assertj.core.api.Assertions.assertThat
+import static smolok.lib.common.Uuids.uuid
 import static smolok.lib.docker.Container.container
 import static smolok.lib.docker.ServiceStartupResults.alreadyRunning
 import static smolok.lib.docker.ServiceStartupResults.created
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = KapuaApplication.class)
+@RunWith(SpringRunner)
+@SpringBootTest(classes = KapuaApplication)
 class DockerConfigurationTest {
+
+    // Fixtures
 
     @Autowired
     Docker docker
 
-    def containerName = randomUUID().toString()
+    def containerName = uuid()
 
     // Tests
 
